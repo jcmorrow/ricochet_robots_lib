5127fc839c6fae1a2ba9b7a0e9101a038be81d05
Josh Morrow <josh@jcmorrow.com>
2017-07-14 14:44:47 -0700

    Spec unspecced space methods

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -15,4 +15,34 @@ describe Space do
       expect(space.position).to eq([1, 2])
     end
   end
+
+  describe "#occupied?" do
+    it "defaults to false" do
+      space = Space.new(0, 0)
+
+      expect(space).not_to be_occupied
+    end
+
+    it "is true if the space has an occupant" do
+      space = Space.new(0, 0, occupant: double("Robot"))
+
+      expect(space).to be_occupied
+    end
+  end
+
+  describe "#to_s" do
+    it "defaults to _" do
+      space = Space.new(0, 0)
+
+      expect(space.to_s).to eq("_")
+    end
+
+    context "when the space is occupied" do
+      it "delegates to the occupant" do
+        space = Space.new(0, 0, occupant: double("Robot", to_s: "foo"))
+
+        expect(space.to_s).to eq("foo")
+      end
+    end
+  end
 end
